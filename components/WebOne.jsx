de85f4e665312a343b67d7977f15f942ea9ce3fb
mahedizaman <info.zaman.profile@gmail.com>
2025-12-15 22:01:11 +0600

    project section added

--- components/WebOne.jsx
+++ components/WebOne.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 const WebOne = () => {
   return (
     <Container>
-      <div className="inline-block">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-6">
         <motion.div
           whileHover={{ scale: 1.05 }}
           className="inline-block border-2 rounded-lg p-4 "
@@ -25,15 +25,32 @@ const WebOne = () => {
 
           <div className="border-t-2 pt-3 mt-6">
             <Link
-              href="https://www.google.com/maps?q=Dhaka,Bangladesh"
+              href="https://berryclone.netlify.app/"
               target="_blank"
               rel="noopener noreferrer"
-              className="mt-4 px-4 py-2 rounded bg-purple-500 text-white cursor-pointer"
+              className=" px-4 py-2 rounded bg-purple-500 text-white cursor-pointer"
             >
               Live
             </Link>
           </div>
         </motion.div>
+        <div>
+          <h2 className="font-bold text-gray-400 text-xl sm:text-2xl sm:text-left mb-4">
+            Berry Clone
+          </h2>
+          <p>
+            BerryClone is a modern e-commerce website clone built with{" "}
+            <strong>Next.js </strong> 
+            and <strong>Tailwind CSS</strong> . It features a clean, responsive
+            design where users can browse products, view details, and add items
+            to a dynamic cart that updates instantly. While it does not include
+            authentication, backend, or real payments, the project highlights
+            interactive UI, component-based architecture, and responsive
+            layouts. BerryClone demonstrates my frontend development skills and
+            can be extended into a fully functional e-commerce platform in the
+            future.
+          </p>
+        </div>
       </div>
     </Container>
   );
